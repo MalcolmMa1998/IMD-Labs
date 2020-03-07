ddac2c6ade01219a5cb55e572092448470c8b3ac
Malcolm Ma <MalcolmMa1998@users.noreply.github.com>
2020-03-07 17:03:50 +0800

    Initial commit

---
+++ JavaScript_and_data persistence-20200302/process_text.js
@@ -0,0 +1,32 @@
+function process_text() {
+
+  // Add button and textarea elements
+
+ var txt;
+ var i;
+ var vowels;
+ var counter = 0;
+
+  btn.onclick = function() {
+
+    // Add code to count the number of vowels in the input text
+
+
+
+    // Loop through text to test if each character is a vowel
+
+}
+
+
+// Display the result
+alert(`The text contains ${counter} vowel(s)`)
+
+
+
+
+  };
+
+
+}
+
+process_text();
